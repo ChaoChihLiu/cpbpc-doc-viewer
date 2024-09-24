8ddbb8be5dbf8b4f4eb12dd5f2fe44f0eca4b638
Liu ChaoChih <chaochihliu@gmail.com>
2024-09-24 15:23:40 +0800

    pre-signed url expired in 1 min

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ import {fileURLToPath} from 'url';
 import util from 'util';
 import {GetObjectCommand, ListObjectsV2Command, S3Client} from '@aws-sdk/client-s3';
 import {getSignedUrl} from '@aws-sdk/s3-request-presigner'
-import uuidv4 from 'uuid'
+import {v4 as uuidv4} from 'uuid'
 import _ from 'lodash'
 
 const execPromise = util.promisify(exec);
